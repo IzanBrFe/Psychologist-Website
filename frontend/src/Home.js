f52c9d6b881b55f2c4d2c416daedddc2816f3278
IzanBrFe <ethanbrfe97@gmail.com>
2025-02-05 19:56:18 +0100

    dos paginas v1

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+// src/Home.js
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Bienvenidos al sitio del Psicólogo Jorge</h1>
+    <p>Este es un sitio web donde puedes agendar tus citas.</p>
+  </div>
+);
+
+export default Home;
